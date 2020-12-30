4aa65be374f4daba3fba1e9935400f2729dc55e1
dianakoh <dianakoh94@gmail.com>
2020-12-31 00:07:02 +0900

    #1.1 GET MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,23 @@
 import React from "react";
-export default () => <h1>Home</h1>;
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+
+  if (loading) {
+    return "loading....";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1 key={m.id}>{m.id}</h1>);
+  }
+};
