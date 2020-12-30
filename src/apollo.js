9ed0cbd2ea3d413a20f8109eac335f7bb6ac01ea
dianakoh <dianakoh94@gmail.com>
2020-12-31 03:35:38 +0900

    #2.1 Local State part Two

--- src/apollo.js
+++ src/apollo.js
@@ -6,6 +6,11 @@ const client = new ApolloClient({
     Movie: {
       isLiked: () => false,
     },
+    Mutation: {
+      likeMovie: (_, { id }, { cache }) => {
+        cache.writeData({ id: `Movie:${id}`, data: { isLiked: true } });
+      },
+    },
   },
 });
 
